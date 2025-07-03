b6cb2f86d86512a250a5b356bdbcc0f286deca9c
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-07-03 16:43:31 +0900

    Refactor: separate local DB config into application-local.yml

--- src/main/java/com/example/blue_archive/dto/boardcomment/BoardCommentUpdateRequest.java
+++ src/main/java/com/example/blue_archive/dto/boardcomment/BoardCommentUpdateRequest.java
@@ -1,4 +1,13 @@
 package com.example.blue_archive.dto.boardcomment;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BoardCommentUpdateRequest {
+
+    @NotBlank(message = "댓글 내용은 필수입니다.")
+    private String content;
 }
